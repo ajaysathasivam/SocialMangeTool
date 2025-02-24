ba3555612f6f67fbaaaeb5b3fad18168f59b9d65
ajaysathasivam <ajay555ko@gmail.com>
2025-02-24 16:34:41 +0530

    new login

--- socialManageTool/src/App.jsx
+++ socialManageTool/src/App.jsx
@@ -1,26 +1,68 @@
-import "./App.css";
+import React from 'react';
 import FacebookLogin from 'react-facebook-login';
-function App() {
-  const responseFacebook = (response) => {
-    console.log(response);
-  }
-  return (
-    <>
-      <h1>Vite + React</h1>
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <FacebookLogin
-        appId="589346337592039"
-        autoLoad={true}
-        fields="name,email,picture"
-        callback={responseFacebook}
-        cssClass="my-facebook-button-class"
-        
-      />
-    </>
+const App = (props) => {
+  const handleFacebookCallback = (response) => {
+    if (response?.status === "unknown") {
+        console.error('Sorry!', 'Something went wrong with facebook Login.');
+     return;
+    }
+    console.log(response);
+      // console will print following object for you.
+       
+   }
+
+  return (
+    <FacebookLogin 
+      buttonStyle={{padding:"6px"}}  
+      appId="1320716502478119"  // we need to get this from facebook developer console by setting the app.
+      autoLoad={false}  
+      
+      callback={handleFacebookCallback}/>
   );
-}
+};
+
+
+
+
+
+// import "./App.css";
+// import FacebookLogin from 'react-facebook-login';
+// function App() {
+//   const responseFacebook = (response) => {
+//     console.log(response);
+//   }
+//   const publishPost = async (accessToken) => {
+//     try {
+//       const response = await fetch(
+//         `https://graph.facebook.com/v18.0/589346337592039/feed`,
+//         {
+//           message: "Hello, this is an automated post from React!",
+//           access_token: accessToken,
+//         }
+//       );
+//       console.log("Post ID:", response.data.id);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+//   return (
+//     <>
+//       <h1>Vite + React</h1>
+
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//       <FacebookLogin
+//         appId="544872944619572"
+//         autoLoad={true}
+        
+//         callback={responseFacebook}
+//         cssClass="my-facebook-button-class"
+        
+//       />
+//     </>
+//   );
+// }
 
 export default App;
