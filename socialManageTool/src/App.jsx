db6313b8cf2d4c3a70d64f2f81f1224a71faf5f9
ajaysathasivam <ajay555ko@gmail.com>
2025-02-24 15:47:19 +0530

    new button

--- socialManageTool/src/App.jsx
+++ socialManageTool/src/App.jsx
@@ -1,18 +1,26 @@
-
-import './App.css'
-
+import "./App.css";
+import FacebookLogin from 'react-facebook-login';
 function App() {
-
+  const responseFacebook = (response) => {
+    console.log(response);
+  }
   return (
     <>
-      
       <h1>Vite + React</h1>
-      
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <FacebookLogin
+        appId="589346337592039"
+        autoLoad={true}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        cssClass="my-facebook-button-class"
+        
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
